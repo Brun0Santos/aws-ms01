61fea8f8d0703c41c3055cfef330f33cb16fa79d
Bruno Santos <bruno_st17@hotmail.com>
2023-12-26 20:22:48 -0300

    feat: adicionando a primeira controller de teste

---
+++ src/main/java/com/bruno/aws_ms01/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.bruno.aws_ms01.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/api")
+public class TestController {
+
+    @GetMapping("/status")
+    public ResponseEntity<String> statusApi() {
+        return ResponseEntity.ok().body("Server is running...");
+    }
+}
