91457c1444c96b7ab109d94188626ad71a5d4de5
Bruno Santos <bruno_st17@hotmail.com>
2023-12-26 20:15:57 -0300

    feat: primeiras configuracoes

---
+++ src/main/java/com/bruno/aws_ms01/AwsMs01Application.java
@@ -0,0 +1,13 @@
+package com.bruno.aws_ms01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsMs01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsMs01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bruno/aws_ms01/AwsMs01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.aws_ms01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsMs01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
